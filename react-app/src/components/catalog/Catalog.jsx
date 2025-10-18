c50228946a2f5aa82c98e85f93489a409a260b71
linxxxa <valisagavrilova65@gmail.com>
2025-10-18 13:53:22 +0300

    Second version

---
+++ react-app/src/components/catalog/Catalog.jsx
@@ -0,0 +1,19 @@
+// src/components/catalog/CatalogSidebar.jsx
+import './Catalog.css';
+
+const Catalog = () => {
+  return (
+    <div className="catalog-sidebar">
+      <h2 className="catalog-title">Каталог</h2>
+      
+      <div className="catalog-content">
+        <div className="catalog-placeholder">
+          <p>Категории товаров</p>
+          <p>появятся здесь</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Catalog;
