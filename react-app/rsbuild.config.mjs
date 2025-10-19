d4c16b7dda3d4d73bf265b58d1814bf869d72b44
linxxxa <valisagavrilova65@gmail.com>
2025-10-19 11:54:21 +0300

    6 version

--- react-app/rsbuild.config.mjs
+++ react-app/rsbuild.config.mjs
@@ -3,6 +3,11 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  source: {
+    entry: {
+      index: './app/index.jsx'
+    }
+  },
   server: {
     proxy: {
       '/api': {
